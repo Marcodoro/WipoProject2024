56db531ce9ea6534571c8cf11ffc5b7d55cd6d8b
Marcodoro <118012046+Marcodoro@users.noreply.github.com>
2024-11-15 15:02:50 +0100

    Readme.md update

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+  apiKey: "AIzaSyCgGMejSjqS13o8yoQoDxECL-Q1nolE_KU",
+  authDomain: "wipoproject2024.firebaseapp.com",
+  projectId: "wipoproject2024",
+  storageBucket: "wipoproject2024.firebasestorage.app",
+  messagingSenderId: "1066791939736",
+  appId: "1:1066791939736:web:84b8e9c4dba28747886aed",
+  measurementId: "G-DWHJCW72DJ"
 };
 
 // Initialize Firebase
