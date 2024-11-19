c1c529d4a1c3703ae9ca3062917b55ed8b307aa7
Marcodoro <118012046+Marcodoro@users.noreply.github.com>
2024-11-19 20:42:40 +0100

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -11,3 +11,12 @@ const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export { db };
+
+/**
+ *  npm i --save @fortawesome/fontawesome-svg-core
+ *  npm i --save @fortawesome/free-solid-svg-icons
+    npm i --save @fortawesome/free-regular-svg-icons
+    npm i --save @fortawesome/free-brands-svg-icons
+    npm install gsap
+    npm install firebase
+ */
